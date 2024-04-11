4783432634faf4690fb9cae3da2025e15c3630ef
Kevin <kevin.barr88@gmail.com>
2024-04-11 11:32:59 +0100

    Update server.js

--- server/assets/js/server.js
+++ server/assets/js/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { getAllMessages, getRandom } from "./route_get";
+import { getAllMessages, getRandom } from "./getHandler";
 
 const app = express();
 app.use(cors());
