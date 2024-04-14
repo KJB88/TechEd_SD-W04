31bde3dfd55a2e575d714b05628b7128914806bb
Kevin <kevin.barr88@gmail.com>
2024-04-14 12:07:11 +0100

    Client-side !response.ok handling

--- client/assets/js/networkHandler.js
+++ client/assets/js/networkHandler.js
@@ -10,7 +10,10 @@ const apiURL = "https://teched-sd-w04.onrender.com";
 // #region REQUESTS
 
 /* GET all messages from remote */
-export async function getMessages(callback) {
+export async function getMessages(
+  successCallback,
+  failureCallback = onFailureCallback
+) {
   const resp = await fetch(`${apiURL}/message`, {
     method: "GET",
     headers: {
@@ -18,13 +21,22 @@ export async function getMessages(callback) {
     },
   });
 
-  const data = await resp.json();
-  console.log("GET response received.");
-  callback(data);
+  if (resp.status == resp.ok) {
+    const data = await resp.json();
+    console.log("GET response received.");
+    successCallback(data);
+  } else {
+    console.log(`GET request failed: ${resp.status}`);
+    failureCallback(resp.status);
+  }
 }
 
 /* POST a new message to remote */
-export async function postMessage(postContent, callback) {
+export async function postMessage(
+  postContent,
+  successCallback,
+  failureCallback = onFailureCallback
+) {
   const resp = await fetch(`${apiURL}/message`, {
     method: "POST",
     body: JSON.stringify(postContent),
@@ -33,13 +45,22 @@ export async function postMessage(postContent, callback) {
     },
   });
 
-  const data = await resp.json();
-  console.log("POST response received.");
-  callback(data);
+  if (resp.status == resp.ok) {
+    const data = await resp.json();
+    console.log("POST response received.");
+    successCallback(data);
+  } else {
+    console.log(`POST request failed: ${resp.status}`);
+    failureCallback(resp.status);
+  }
 }
 
 /* DELETE a message from remote (based on ID) */
-export async function deleteMessage(id, callback) {
+export async function deleteMessage(
+  id,
+  successCallback,
+  failureCallback = onFailureCallback
+) {
   const resp = await fetch(`${apiURL}/message`, {
     method: "DELETE",
     body: JSON.stringify({ id: `${id}` }),
@@ -48,13 +69,22 @@ export async function deleteMessage(id, callback) {
     },
   });
 
-  const data = await resp.json();
-  console.log(data);
-  callback(data);
+  if (resp.status == resp.ok) {
+    const data = await resp.json();
+    console.log("DEL response received.");
+    successCallback(data);
+  } else {
+    console.log(`DEL request failed: ${resp.status}`);
+    failureCallback(resp.status);
+  }
 }
 
 /* PUT a like on the message, specified by ID, on the remote */
-export async function putLike(id, callback) {
+export async function putLike(
+  id,
+  successCallback,
+  failureCallback = onFailureCallback
+) {
   const resp = await fetch(`${apiURL}/like`, {
     method: "PUT",
     body: JSON.stringify({ id: `${id}` }),
@@ -63,10 +93,17 @@ export async function putLike(id, callback) {
     },
   });
 
-  const data = await resp.json();
-  console.log(data);
-  callback(data);
+  if (resp.status == resp.ok) {
+    const data = await resp.json();
+    console.log("PUT response received.");
+    successCallback(data);
+  } else {
+    failureCallback(resp.status);
+  }
 }
 
+function onFailureCallback(status) {
+  console.log(`PUT request failed: ${status}`);
+}
 // #endregion REQUESTS
 /* -------------------- */
